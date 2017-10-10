9ae4546d05f7243769b5842c7eb05bf6ca79f285
Hayat Mazzarella <imhighyat@gmail.com>
2017-10-10 01:57:07 -0700

    added endpoint for recipes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +18,19 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+// adding some data to the recipelist
+Recipes.create('fried rice', ['rice', 'egg', 'garlic', 'oil']);
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('vodka sprite', ['vodka', 'sprite', 'cherry']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
